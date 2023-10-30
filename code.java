af3534c20bea5e4b593476bdc5663602b4586883
VikasSinga <126710749+VikasSinga@users.noreply.github.com>
2023-10-30 11:16:39 +0530

    Update code.java
    diamond pattern

--- code.java
+++ code.java
@@ -1,5 +1,60 @@
-public class code {
+import java.util.Scanner;
+
+public class DiamondPattern {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the diamond: ");
+        int n = input.nextInt();
+        input.close();
+
+        if (n % 2 == 0) {
+            System.out.println("Please enter an odd number for a proper diamond pattern.");
+            return;
+        }
+
+        int spaces = n / 2;
+        int stars = 1;
+
+        // Print the top half of the diamond
+        for (int i = 1; i <= n / 2 + 1; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+
+            spaces--;
+            stars += 2;
+        }
+
+        // Print the bottom half of the diamond
+        spaces = 1;
+        stars = n - 2;
+
+        for (int i = 1; i <= n / 2; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+
+            spaces++;
+            stars -= 2;
+        }
     }
 }
+
