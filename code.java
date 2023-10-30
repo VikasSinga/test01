7d8009be27d7c3dbe70fa60dcdc113d80ad13649
VikasSinga <126710749+VikasSinga@users.noreply.github.com>
2023-10-30 11:19:58 +0530

    Update code.java
    main changed

--- code.java
+++ code.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DiamondPattern {
+public class code {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of rows for the diamond: ");
